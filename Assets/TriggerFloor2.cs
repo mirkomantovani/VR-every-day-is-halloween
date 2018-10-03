061d9a866d051684b5411f6752ba0dc3a3aa5ec5
Mirko Mantovani <mirko.mantovani@mail.polimi.it>
2018-10-03 12:34:46 -0500

    Changed everything after first VIVE trial, created rooms, basic animations for heaven stairs and floor

---
+++ Assets/TriggerFloor2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFloor2 : MonoBehaviour {
+
+    public Animator floor2;
+    public Animator heavenStairs;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered in cereal1 trigger: " + other.name);
+
+        switch (other.name)
+        {
+            case "[VRTK][AUTOGEN][FootColliderContainer]":
+                Debug.Log("Playing animation Room2FloorAppear");
+                floor2.Play("Room2FloorAppear");
+                heavenStairs.Play("HeavenStairsFade");
+                //heavenStairs.Play("none");
+                break;
+            default:
+                Debug.Log("Unidentified object entered");
+                break;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TriggerHeaven.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHeaven : MonoBehaviour {
+
+    public Animator heavenStairs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered in cereal1 trigger: " + other.name);
+
+        switch (other.name)
+        {
+            case "FlameSkull":
+                Debug.Log("Playing animation HeavenStairsAppear");
+                heavenStairs.Play("HeavenStairsAppear");
+                //heavenStairs.Play("none");
+                break;
+            default:
+                Debug.Log("Unidentified object entered");
+                break;
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
